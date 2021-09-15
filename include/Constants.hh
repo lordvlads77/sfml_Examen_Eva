249ed0d2907f8fdf0b015470b166a3958c093146
lordvlads77 <81182279+lordvlads77@users.noreply.github.com>
2021-09-15 10:54:11 -0500

    Se hizo lo que se pudo, se tenia la intencion

--- include/Constants.hh
+++ include/Constants.hh
@@ -1,5 +1,5 @@
 #pragma once
 
-const unsigned int WINDOW_WIDTH{1280};
-const unsigned int WINDOW_HEIGHT{720};
+const unsigned int WINDOW_WIDTH{1600};
+const unsigned int WINDOW_HEIGHT{990};
 const char* GAME_NAME{"El Solecito de Andromeda"};
